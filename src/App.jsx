1454109387e1db33df81f425fd24656c00421976
Joana <144175120+diverintech@users.noreply.github.com>
2025-01-22 15:06:23 +0000

    Simplify App.jsx html

--- src/App.jsx
+++ src/App.jsx
@@ -92,154 +92,49 @@ function App() {
         <>
             <section className="items">
                 <h4>{shopName}</h4>
-                
-                <div className="product selected">
-                    <div className="photo">
-                        <img src="././img/real_madrid.webp" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Real Madrid</span>
-                        <span className="price">$ 119.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
+
+                { items.map((item) =>
+                    <div key={item.id} className="product selected">
+                        <div className="photo">
+                            <img src={"././img/" + item.photo}/>
+                        </div>
+                        <div className="description">
+                            <span className="name">{item.name}</span>
+                            <span className="price">{item.price}</span>
+                            {
+                                item.isInBag ?
+                                    <div className="quantity-area">
+                                        <button>-</button>
+                                        <span className="quantity">{item.quantity}</span>
+                                        <button>+</button>
+                                    </div>
+                                    : ''
+                            }
                         </div>
                     </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/milan.png" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Milan</span>
-                        <span className="price">$ 99.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/chelsea.webp" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Chelsea</span>
-                        <span className="price">$ 99.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/barcelona.png" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Barcelona</span>
-                        <span className="price">$ 109.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/benfica.png" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Benfica</span>
-                        <span className="price">$ 89.49</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/manchester.webp" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Manchester City</span>
-                        <span className="price">$ 129.79</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/bayern.webp" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Bayern</span>
-                        <span className="price">$ 119.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/psg.png" />
-                    </div>
-                    <div className="description">
-                        <span className="name">PSG</span>
-                        <span className="price">$ 94.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/ajax.webp" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Ajax</span>
-                        <span className="price">$ 89.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
+                )}
+
+
             </section>
-            
+
 
             <section className="summary">
                 <strong>Order Details</strong>
                 <table>
                     <thead>
-                        <tr>
-                            <th>Item</th>
-                            <th>Total</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1x Real Madrid</td>
-                            <td>$ 119.99</td>
-                        </tr>
-                        
-                        <tr>
-                            <th>Total</th>
+                    <tr>
+                        <th>Item</th>
+                        <th>Total</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1x Real Madrid</td>
+                <td>$ 119.99</td>
+            </tr>
+
+            <tr>
+            <th>Total</th>
                             <th>$ 119.99</th>
                         </tr>
                     </tbody>
